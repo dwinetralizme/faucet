fa302a863b22d58a62d0151e738c06fce1882bfa
unknown <dwinetralizme@gmail.com>
2022-08-10 00:50:15 +0700

    implement bulma

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
-
 function App() {
   return (
     <div className="App">
-      Hello World
+      <div class="card">
+        <header class="card-header">
+          <p class="card-header-title">Card header</p>
+          <button class="card-header-icon" aria-label="more options">
+            <span class="icon">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
+        </header>
+      </div>
     </div>
   );
 }
