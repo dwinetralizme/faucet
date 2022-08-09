86d3a174f4555ab38e87868bc2a84bb65ccdb6b2
unknown <dwinetralizme@gmail.com>
2022-08-10 01:16:04 +0700

    initial css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+import "./App.css";
+
 function App() {
   return (
-    <div className="App">
-      <div class="card">
-        <header class="card-header">
-          <p class="card-header-title">Card header</p>
-          <button class="card-header-icon" aria-label="more options">
-            <span class="icon">
-              <i class="fas fa-angle-down" aria-hidden="true"></i>
-            </span>
-          </button>
-        </header>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn">Withdraw</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
