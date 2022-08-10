1ae7dab776b68a20a74d11695c0a9e0adc66e239
wln <dwi.wulantoro@infoglobal.co.id>
2022-08-10 16:21:28 +0700

    add web3 and implement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
+import React from "react";
 import "./App.css";
+import {useEffect,useState} from "react";
+import Web3 from "web3";
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null
+  })
+  useEffect(()=>{
+    const loadProvider = async()=>{
+      let provider = null;
+      if(window.ethereum){
+        provider = window.ethereum;
+
+        console.log(provider);
+        try {
+          await provider.request({ method: 'eth_requestAccounts' });
+        } catch (error) {
+          console.error(error);
+        }
+      }else if(window.web3){
+        provider = window.web3.currentProvider
+      }else if(!process.env.production){
+        provider = new Web3.providers.HttpProvider("http://localhost:7545");
+      }
+
+      setWeb3Api({
+        web3: new Web3(provider),
+        provider
+      })
+    }
+
+    loadProvider()
+
+  },[]);
+
+  console.log(web3Api.web3);
+
+
   return (
     <>
       <div className="faucet-wrapper">
